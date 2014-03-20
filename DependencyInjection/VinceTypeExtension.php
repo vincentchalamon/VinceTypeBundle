484d9ab80554a91b9a4cdf940346c92c9f2f657b
Vincent CHALAMON <vincentchalamon@gmail.com>
2014-03-20 14:59:06 +0100

    Update for assetic

--- DependencyInjection/VinceTypeExtension.php
+++ DependencyInjection/VinceTypeExtension.php
@@ -12,6 +12,7 @@ namespace Vince\Bundle\TypeBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -20,7 +21,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class VinceTypeExtension extends Extension
+class VinceTypeExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
@@ -38,5 +39,46 @@ class VinceTypeExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $bundles = $container->getParameter('kernel.bundles');
+
+        // Configure Assetic if AsseticBundle is activated
+        if (isset($bundles['AsseticBundle']) && $container->hasExtension('assetic')) {
+            $container->prependExtensionConfig('assetic', array(
+                    'bundles' => array('VinceTypeBundle'),
+                    'assets' => array(
+                        'vince_type_js' => array(
+                            'inputs' => array(
+                                '@VinceTypeBundle/Resources/public/datepicker/bootstrap-datepicker.js',
+                                '@VinceTypeBundle/Resources/public/redactor/redactor.js',
+                                '@VinceTypeBundle/Resources/public/masked/jquery.maskedinput.min.js',
+                                '@VinceTypeBundle/Resources/public/token/jquery.tokeninput.js',
+                                '@VinceTypeBundle/Resources/public/list/listInput.js'
+                            ),
+                            'filters' => $container->hasParameter('assetic.filter.yui_js.jar') ? array('?yui_js') : array(),
+                            'output' => '/js/vince_type.js'
+                        ),
+                        'vince_type_css' => array(
+                            'inputs' => array(
+                                '@VinceTypeBundle/Resources/public/datepicker/bootstrap.datepicker.min.css',
+                                '@VinceTypeBundle/Resources/public/datepicker/datepicker.css',
+                                '@VinceTypeBundle/Resources/public/redactor/redactor.css',
+                                '@VinceTypeBundle/Resources/public/token/token-input.css',
+                                '@VinceTypeBundle/Resources/public/token/token-input-facebook.css',
+                                '@VinceTypeBundle/Resources/public/list/listInput.css'
+                            ),
+                            'filters' => array_merge(array('cssrewrite'), $container->hasParameter('assetic.filter.yui_css.jar') ? array('?yui_css') : array()),
+                            'output' => '/css/vince_type.css'
+                        )
+                    )
+                )
+            );
+        }
     }
 }
