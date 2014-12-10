07a9bd3ad55b77c9024a186fc4c4b971db77f368
Vincent Chalamon <vincentchalamon@gmail.com>
2014-12-10 13:08:34 +0100

    phpcs

--- Twig/Extension/FormExtension.php
+++ Twig/Extension/FormExtension.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the VinceType bundle.
  *
- * (c) Vincent Chalamon <vincentchalamon@gmail.com>
+ * (c) Vincent Chalamon <http://www.vincent-chalamon.fr>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
